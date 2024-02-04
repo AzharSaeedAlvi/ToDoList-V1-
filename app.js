f1b78504610ff4446db5e3ad42401a44095f565d
Azhar Alvi <96597385+AzharSaeedAlvi@users.noreply.github.com>
2024-02-04 23:21:12 +0530

    Add files via upload
    basic file where - Passing Data from Your Webpage to Your Server is being done

---
+++ app.js
@@ -0,0 +1,42 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+var items =["food1","Food2","Food3"];
+
+app.set("view engine", "ejs"); 
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  
+  var today = new Date();
+
+  var options ={
+weekday: "long",
+month:"long",
+day:"numeric" 
+  };
+
+var day= today.toLocaleDateString("en-US",options);
+  
+
+
+    res.render("list", {KindOfDay: day, NewListItems: items });
+  });
+
+  app.post("/", function(req,res){
+   item = req.body.newItem;                                  // Before setting up body we need to keep in mind that we should have setup body-parser.
+  console.log(items);
+
+  res.redirect("/");
+
+});
+ 
+
+
+app.listen(3000, function(){ 
+  console.log("Server started on port 3000");
+});
